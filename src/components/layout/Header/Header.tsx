df7273c6bebcf253dd7756b483f6a28fea477175
Viktor-Kl <viktorkyrylovych88@gmail.com>
2023-05-01 19:57:55 +0300

    Add responsive to components, fix some moments

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,13 +1,21 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import Link from "next/link";
 
-import Button from 'components/ui/Button/Button'
+import Button from "components/ui/Button/Button";
 
 import navData from "data/navData.json";
 import styles from "./Header.module.scss";
 
+import { IoIosArrowForward } from "react-icons/io";
+import { RxCross2, RxHamburgerMenu } from "react-icons/rx";
+
 const Header: FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
   const list: INavData[] = navData.navigation;
+
+  const togleMenu = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <header className={styles.wrapper} id="header">
@@ -23,10 +31,34 @@ const Header: FC = () => {
               </a>
             ))}
           </div>
-          <Button type='anchor' text='Меню' href='#menu' className="ghost-btn" customClass={styles.button}/>
+          <div className={styles.btnPart}>
+            <Button
+              type="anchor"
+              text="Меню"
+              href="#menu"
+              className="ghost-btn"
+              customClass={styles.button}
+            />
+            <button onClick={togleMenu}>
+              {isOpen ? (
+                <RxCross2 className={styles.close} />
+              ) : (
+                <RxHamburgerMenu className={styles.open} />
+              )}
+            </button>
+          </div>
         </nav>
         <div className={styles.line}></div>
       </div>
+      {isOpen && (
+        <div className={styles.mobileLinksWrap}>
+          {list.map((item, key) => (
+            <Link key={key} href={item.href} className={styles.mobileLink}>
+              {item.text} <IoIosArrowForward />
+            </Link>
+          ))}
+        </div>
+      )}
     </header>
   );
 };
--- src/components/screens/Offer/Offer.tsx
+++ src/components/screens/Offer/Offer.tsx
@@ -1,20 +1,20 @@
 import { FC } from "react";
 import Image from "next/image";
 
-import styles from './Offer.module.scss'
+import styles from "./Offer.module.scss";
 import OfferContent from "./OfferContent";
 
 const Offer: FC = () => {
-    return (
-        <section className={styles.section} id="offer">
-            <div className="container">
-                <div className={styles.wrapper}>
-                    <OfferContent />
-                    <Image src='/img/offer/img-2.png' width={486} height={486} alt='dish-img'/>
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section className={styles.section} id="offer">
+      <div className="container">
+        <div className={styles.wrapper}>
+          <OfferContent />
+          <div className={styles.image}></div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Offer;
