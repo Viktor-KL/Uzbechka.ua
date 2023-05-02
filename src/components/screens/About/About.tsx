cf277da15cab2ce21110de2572f47c44ce064d70
Viktor-Kl <viktorkyrylovych88@gmail.com>
2023-05-02 10:23:05 +0300

    Create About component

---
+++ src/components/screens/About/About.tsx
@@ -0,0 +1,41 @@
+import { FC } from "react";
+import Image from "next/image";
+
+import aboutData from "data/aboutData.json";
+import styles from "./About.module.scss";
+
+const About: FC = () => {
+  return (
+    <section className={styles.section}>
+      <div className="container">
+        <div className={styles.wrapper}>
+          <p className={styles.description}>
+            {aboutData.firstPart}
+            <span>
+              <Image
+                src="/img/about/img-1.png"
+                width={260}
+                height={122}
+                alt="dish-image"
+                className={styles.image}
+              />
+            </span>
+            {aboutData.secondPart}
+            <span>
+              <Image
+                src="/img/about/img-2.png"
+                width={260}
+                height={122}
+                alt="dish-image"
+                className={styles.image}
+              />
+            </span>
+            {aboutData.thirdPart}
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import type { NextPage } from 'next'
 import Offer from 'components/screens/Offer/Offer';
+import About from 'components/screens/About/About';
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,7 @@ const Home: NextPage = () => {
         <title>Home | Uzbechka.ua</title>
       </Head>
       <Offer />
+      <About />
     </div>
   )
 }
